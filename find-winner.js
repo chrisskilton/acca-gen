4b8a9504c1574e26ef330b1e4a4c48a3ae199d18
Chris Skilton <chriss@brandwatch.com>
2016-02-10 09:12:35 +0000

    removed the async from find-winners as results looked odd

--- find-winner.js
+++ find-winner.js
@@ -1,14 +1,14 @@
 var generate = require('./generate-picks');
 var winners = [
-  { home: 'Sunderland', away: 'Man Utd', result: 'H' },
-  { home: 'Bournemouth', away: 'Stoke', result: 'A' },
-  { home: 'Crystal Palace', away: 'Watford', result: 'H' },
-  { home: 'Everton', away: 'West Brom', result: 'D' },
-  { home: 'Swansea', away: 'Southampton', result: 'H' },
-  { home: 'Chelsea', away: 'Newcastle', result: 'H' }
+  { home: 'Tottenham', away: 'Watford', result: 'H' },
+  { home: 'Southampton', away: 'West Ham', result: 'H' },
+  { home: 'Aston Villa', away: 'Norwich', result: 'H' },
+  { home: 'Liverpool', away: 'Sunderland', result: 'D' },
+  { home: 'Newcastle', away: 'West Brom', result: 'H' },
+  { home: 'Stoke', away: 'Everton', result: 'A' }
   ];
 
-function findWinner(currentCount, callback) {
+function findWinner(currentCount) {
     var picks = generate();
     var winner = true;
     var count;
@@ -33,30 +33,25 @@ function findWinner(currentCount, callback) {
     });
 
     if (!winner) {
-        return process.nextTick(findWinner.bind(null, count, callback));
+        return findWinner(count);
     }
 
-    //console.log(count, 'tickets generated to find winner');
-    callback(count);
+    return count;
 }
 
 var results = [];
 var sampleSize = 100;
 
 for (var i = 0; i < sampleSize; i++) {
-    findWinner(undefined, function(count) {
-        results.push(count);
-
-        if (results.length !== sampleSize) {
-            return;
-        }
-
-        var average = results.reduce(function(memo, num) {
-            memo += num;
-
-            return memo;
-        }, 0) / sampleSize;
-
-        console.log(average, 'average');
-    });
+    results.push(findWinner());
 }
+
+var average = results.reduce(function(memo, num) {
+    memo += num;
+
+    return memo;
+}, 0) / sampleSize;
+
+console.log(average, 'average');
+console.log(Math.max.apply(null, results), 'max');
+console.log(Math.min.apply(null, results), 'min');
